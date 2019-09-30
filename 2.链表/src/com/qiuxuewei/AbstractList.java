14d2d50cf2b206811dba2eaea481fa0f94829166
qiuxuewei <qiuxuewei@peiwo.cn>
2019-09-30 13:09:33 +0800

    feat: 抽象类

---
+++ 2.链表/src/com/qiuxuewei/AbstractList.java
@@ -0,0 +1,48 @@
+package com.qiuxuewei;
+
+public abstract class AbstractList <E> implements List<E> {
+	protected int size;
+	@Override
+	public int size() 
+	{
+		return size;
+	}
+	
+	@Override
+	public boolean isEmpty() 
+	{
+		return size == 0;
+	}
+	
+	@Override
+	public boolean contains(E element) 
+	{
+		return indexOf(element) != ELEMENT_NOT_FOUND;
+	}
+	
+	@Override
+	public void add(E element) 
+	{
+		add(size, element);
+	}
+	
+	protected void outOfBounds(int index)
+	{
+		throw new IndexOutOfBoundsException("Index:" + index + "Size:" + size);
+	}
+	
+	protected void rangeCheck(int index)
+	{
+		if (index < 0 || index >= size) {
+			outOfBounds(index);
+		}
+	}
+	
+	protected void rangCheckForAdd(int index)
+	{
+		if (index < 0 || index > size) {
+			outOfBounds(index);
+		}
+	}
+	
+}
