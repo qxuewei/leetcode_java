31e90637fd460de99dbd376bfc44770992f521f4
qiuxuewei <qiuxuewei@peiwo.cn>
2020-11-26 11:12:44 +0800

    update

--- src/剑指Offer/_46_把数字翻译成字符串.java
+++ src/剑指Offer/_46_把数字翻译成字符串.java
@@ -29,7 +29,7 @@ public class _46_把数字翻译成字符串 {
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
 
-    public int translateNum(int num) {
-        return 0;
-    }
+//    public int translateNum(int num) {
+//        return 0;
+//    }
 }
