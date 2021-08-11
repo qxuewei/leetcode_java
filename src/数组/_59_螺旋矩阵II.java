087b563889b454cd71f71cf4a9ed8dfa9b3c643c
qiuxuewei <qiuxuewei@baidu.com>
2021-08-11 20:38:12 +0800

    59

--- src/数组/_59_螺旋矩阵II.java
+++ src/数组/_59_螺旋矩阵II.java
@@ -27,4 +27,33 @@ public class _59_螺旋矩阵II {
      * 链接：https://leetcode-cn.com/problems/spiral-matrix-ii
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
+
+    public int[][] generateMatrix(int n) {
+        int[][] res = new int[n][n];
+        int l = 0, r = n - 1, t = 0, b = n - 1;
+        int num = 1, target = n * n;
+        while (num <= target) {
+            // 左 -> 右
+            for (int i = l; i <= r; i++) {
+                res[t][i] = num++;
+            }
+            t++;
+            // 上 -> 下
+            for (int i = t; i <= b; i++) {
+                res[i][r] = num++;
+            }
+            r--;
+            // 右 -> 左
+            for (int i = r; i >= l ; i--) {
+                res[b][i] = num++;
+            }
+            b--;
+            // 下 -> 上
+            for (int i = b; i >= t ; i--) {
+                res[i][l] = num++;
+            }
+            l++;
+        }
+        return res;
+    }
 }
