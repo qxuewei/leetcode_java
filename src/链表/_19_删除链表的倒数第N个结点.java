d3c0dd7b208fb8b397f80cf06e05e90c889b1cf4
qiuxuewei <qiuxuewei@baidu.com>
2021-08-20 13:25:28 +0800

    19

---
+++ src/链表/_19_删除链表的倒数第N个结点.java
@@ -0,0 +1,60 @@
+package 链表;
+
+public class _19_删除链表的倒数第N个结点 {
+    /**
+     * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+     *
+     * 进阶：你能尝试使用一趟扫描实现吗？
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     *
+     * 输入：head = [1,2,3,4,5], n = 2
+     * 输出：[1,2,3,5]
+     * 示例 2：
+     *
+     * 输入：head = [1], n = 1
+     * 输出：[]
+     * 示例 3：
+     *
+     * 输入：head = [1,2], n = 1
+     * 输出：[1]
+     *  
+     *
+     * 提示：
+     *
+     * 链表中结点的数目为 sz
+     * 1 <= sz <= 30
+     * 0 <= Node.val <= 100
+     * 1 <= n <= sz
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return null;
+        }
+        if (n == 0) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode slow = dummyHead;
+        ListNode fast = dummyHead;
+        for (int i = 0; i < n; i++) {
+            if (fast == null) return head;
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummyHead.next;
+    }
+}
